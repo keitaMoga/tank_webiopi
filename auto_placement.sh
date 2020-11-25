b8b2186839a0e21b380e1ad8d7086b0d6b22c839
Keita MOGAKI <keitamoga@hotmail.com>
2020-11-25 14:19:56 +0000

    add backup process

--- auto_placement.sh
+++ auto_placement.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-echo hello
+. ./dir_conf.txt
+
+if [ ! -e $BACKUP_DIR ]; then
+    mkdir $BACKUP_DIR
+
+    if [ -e $INDEX_DIR ]; then
+       sudo cp -i $INDEX_DIR $BACKUP_DIR/
+    fi
+    if [ -e $CONFIG_DIR ]; then
+       sudo cp -i $CONFIG_DIR $BACKUP_DIR/
+    fi
+    if [ -e $MAIN_CODE_DIR ]; then
+       sudo cp -i $MAIN_CODE_DIR $BACKUP_DIR/
+    fi
+fi
+
+echo $INDEX_DIR
